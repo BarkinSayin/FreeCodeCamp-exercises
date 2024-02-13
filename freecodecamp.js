1ec9dd01d104c07c36749fcc3c0c9838daa12abf
BarkinSayin <barkinsayin@hotmail.com>
2024-02-13 19:24:15 +0300

    create freecodecamp exercises

---
+++ freecodecamp.js
@@ -0,0 +1,159 @@
+//Golf Code
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes === par - 1) {
+    return "Birdie";
+  } else if (strokes === par) {
+    return "Par";
+  } else if (strokes === par + 1) {
+    return "Bogey";
+  } else if (strokes === par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+}
+
+console.log(golfScore(4, 1));
+console.log(golfScore(4, 2));
+console.log(golfScore(4, 3));
+console.log(golfScore(4, 4));
+console.log(golfScore(4, 5));
+console.log(golfScore(4, 6));
+console.log(golfScore(4, 7));
+
+//Counting Cards
+
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
+console.log(cc());
+
+//Record Collection
+
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "") {
+    if (!records[id].hasOwnProperty("tracks")) {
+      records[id][prop] = [value];
+    } else {
+      records[id][prop].push(value);
+    }
+  }
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 2548, "artist", ""));
+
+//Profile Lookup
+
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Sherlock", "likes"));
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Bob", "number"));
+console.log(lookUpProfile("Akira", "address"));
